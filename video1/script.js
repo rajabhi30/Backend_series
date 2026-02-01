75482b381b39f25677c09d9a7cba3865c90216bb
Abhishek <rajabhishek8070@gmail.com>
2026-02-02 01:57:47 +0530

    basic/js

---
+++ video1/script.js
@@ -0,0 +1,58 @@
+// let arr=[1,2,3,4,"hello", function(){}, true];
+
+// arr.forEach(function(){
+//     console.log("helllo jee");
+// })
+
+// let newarr=arr.map(function(val){
+//     return val+10
+// })
+
+// console.log(newarr);
+
+// let farr=arr.filter(function(val){
+//     if(val<5){
+//         return val
+//     }
+// })
+
+// console.log(farr);
+
+// var ans=arr.indexOf(4)
+// console.log(ans);
+
+// var ans1=arr.find(function(val){
+//     if(val==99){
+//         return val;
+//     }
+//     else{
+//         return false;
+//     }
+// });
+
+// console.log(ans1);
+
+
+// let obj={
+//     name:"abhishek",
+//     age:21
+// }
+
+// console.log(obj.name);
+// console.log(obj['name']);
+// Object.freeze(obj)
+// obj.age=100;
+
+// console.log(obj.age);
+
+
+// function abc(a,b,c){
+//     console.log("hello from function");
+// }
+
+// abc(0,0,0);
+
+// let ans=abc.length;  // length of a function is parameter it has
+
+// console.log(ans);
+
