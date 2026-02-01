f506943b0bb8101b310ad948f34494c18c087448
Abhishek <rajabhishek8070@gmail.com>
2026-02-02 02:40:44 +0530

    fileSystem

---
+++ video2/script.js
@@ -0,0 +1,69 @@
+// hello jii
+
+const fs=require('fs');
+
+// fs.writeFile("hey.txt","helli jee kaise ho!!",function(err){
+//     if(err){
+//         console.error(err);
+//     }
+//     else{
+//         console.log("done jee");
+//     }
+// })
+
+// fs.appendFile("hey.txt","i am adding something in file using append", function(err){
+//     if(err){
+//         console.err(err.message);
+//     }
+//     else{
+//         console.log("append ho gya");
+//     }
+// })
+
+// fs.rename("hey.txt","hello.txt",function(err){
+//     if(err){
+//         console.log(err.message);
+//     }
+//     else{
+//         console.log("naam badal gyaa");
+//     }
+// });
+
+
+// fs.copyFile("hello.txt","./copy/chacha.txt",function(err){
+//     if(err){
+//         console.log("malik ek mistake ho gaya")
+//     }
+//     else{
+//         console.log("copy ho gaya bhai")
+//     }
+// })
+
+// fs.unlink("hello.txt",function(err){
+//     if(err){
+//         console.log(err.message);
+//     }
+//     else{
+//         console.log("fuck");
+//     }
+// })
+
+// fs.rmdir("./copy", {recursive:true},function(err){
+//     if(err){
+//         console.log("delete nahi hua");
+//     }
+//     else{
+//         console.log("delete ho gaya")
+//     }
+// })
+
+
+
+fs.rm("./copy", {recursive:true},function(err){
+    if(err){
+        console.log("delete nahi hua");
+    }
+    else{
+        console.log("delete ho gaya")
+    }
+})
