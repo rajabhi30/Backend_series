5edab1b539b218959efccb7ff2897c1243183002
Abhishek <rajabhishek8070@gmail.com>
2026-02-04 01:32:46 +0530

    route/middlewere

---
+++ video4/script.js
@@ -0,0 +1,37 @@
+const express=require('express');
+
+const app=express();
+
+
+//middlewere
+
+app.use(function(req,res,next){
+    console.log("I am in middlewere1")
+    next();
+});
+
+
+
+app.use(function(req,res,next){
+    console.log("I am in middlewere2")
+    next();
+});
+  
+
+
+app.get("/", function(req,res){
+    res.send("this is a homeeee page");
+})
+
+app.get("/profile",function(req,res,next){
+    // res.send("this is a profile page")
+    return next(new Error("not implemented!!"));
+
+})
+
+app.use((err,req,res,next)=>{
+    console.log(err.stack)
+    res.status(500).send("something went wrong");
+})
+
+app.listen(3000);
