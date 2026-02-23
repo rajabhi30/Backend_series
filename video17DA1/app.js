e501fdcd84c07d85cd939fbed14bc7bbb831b25c
Abhishek <rajabhishek8070@gmail.com>
2026-02-23 11:44:24 +0530

    post

---
+++ video17DA1/app.js
@@ -0,0 +1,149 @@
+const express=require('express')
+const app=express();
+const userModel=require('./models/user');
+const postModel=require('./models/post');
+const bcrypt=require('bcrypt')
+const cookieParser = require('cookie-parser');
+const jwt=require('jsonwebtoken')
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+app.get("/profile", isLoggedIN,async(req,res)=>{
+    let user= await userModel.findOne({email: req.user.email}).populate("posts");
+    res.render('profile', {user})
+})
+
+
+app.post("/post", isLoggedIN,async(req,res)=>{
+    let user= await userModel.findOne({email: req.user.email});
+    let {content}=req.body
+   let post = await postModel.create({
+        user: user._id,
+        content: content
+    })
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+})
+
+
+
+app.post('/register', async (req,res)=>{
+    let {email, password, name, username, age} = req.body;
+
+    let user = await userModel.findOne({email});
+
+    if(user){
+        return res.status(400).send("user already registered");
+    }
+
+    bcrypt.genSalt(10, (err,salt)=>{
+        bcrypt.hash(password, salt, async (err,hash)=>{
+
+            let newUser = await userModel.create({
+                username,
+                email,
+                age,
+                name,
+                password: hash
+            });
+
+            let token = jwt.sign(
+                {email: email, userId: newUser._id}, 
+                "secret"
+            );
+
+            res.cookie("token", token);   
+            res.send("registered");
+        });
+    });
+});
+
+
+app.post('/login', async (req,res)=>{
+    let {email, password} = req.body;
+
+    let user = await userModel.findOne({email});
+
+    if(!user){
+        return res.status(400).send("Something went wrong");
+    }
+
+
+    bcrypt.compare(password, user.password, function(err, result){
+        if(result){
+             let token = jwt.sign(
+                {email: email, userId: user._id},
+                "secret"
+            );
+
+            res.cookie("token", token);  
+            return res.status(200).redirect('/profile');
+        }else{
+           return res.redirect("/login");
+        }
+    })
+});
+
+
+app.get("/logout", function(req,res){
+    res.cookie("token", "");
+    res.redirect('/login')
+})
+
+
+// ===============================edit and like code-------------------------------------------
+
+app.get("/edit/:id", isLoggedIN, async (req,res)=>{
+    let post=await postModel.findOne({_id: req.params.id}).populate("user");
+
+    res.render('edit',  {post})
+})
+
+app.post("/update/:id", isLoggedIN, async (req,res)=>{
+    let post=await postModel.findOneAndUpdate({_id: req.params.id},{content: req.body.content})
+    res.redirect('/profile')
+})
+
+app.get("/like/:id", isLoggedIN, async (req,res)=>{
+    let post=await postModel.findOne({_id: req.params.id}).populate("user");
+
+    if(post.likes.indexOf(req.user.userId)==-1){
+         post.likes.push(req.user.userId);
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userId),1);
+    }
+    await post.save();
+    res.redirect('/profile');
+})
+
+
+
+
+function isLoggedIN(req,res,next){
+    if(req.cookies.token==""){
+        return res.redirect("/login");
+    }else{
+        let data=jwt.verify(req.cookies.token,"secret");
+       req.user=data;
+       next();
+    }
+}
+
+
+app.listen(3000);
